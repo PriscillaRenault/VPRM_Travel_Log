5c27c1d808a08e1d6a0fb377e1b676857d5d4b7a
PriscillaRenault <priscilla.renault72460@gmail.com>
2025-03-23 22:53:31 +0100

    footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 //import
 import Header from './components/Header/index.vue'
+import Footer from './components/Footer/index.vue'
 </script>
 
-<template><Header /></template>
+<template>
+  <Header />
+  <Footer />
+</template>
---
+++ frontend/src/components/Footer/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2>Mentions légales</h2>
+</template>
